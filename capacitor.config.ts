f6ff5b670805e69f83f881da7c0c801401187002
Jessica Downes <youremail@example.com>
2026-03-12 13:18:53 -0400

    notifications

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.doknotforget.app',
+  appName: 'DoKnotForget',
+  webDir: 'dist'
+};
+
+export default config;
